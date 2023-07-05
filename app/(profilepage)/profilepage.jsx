a6661dd1c105f0349a194d319959eee1f4cc93c1
Kylie Cheong <kyliecheongg22@gmail.com>
2023-07-05 12:54:08 +0800

    Update Logout on Summary page

--- app/(profilepage)/profilepage.jsx
+++ app/(profilepage)/profilepage.jsx
@@ -30,6 +30,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#e9d3ff'
   },
   backContainer: {
     position: 'absolute',
